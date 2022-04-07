2cd00044643df5b813b1da4371a357001a13c5b0
Noch4nce <m4rgera123@mail.ru>
2022-04-07 20:04:30 +0500

    set intr, integration date html

--- CountdownTimer/script.js
+++ CountdownTimer/script.js
@@ -3,31 +3,20 @@ const hoursSelector = document.querySelector(".hours")
 const minsSelector = document.querySelector(".mins")
 const secondsSelector = document.querySelector(".secs")
 
-// const seconds = new Date().getSeconds()
-const seconds = 22
-secondsSelector.innerHTML = seconds - 1
-
 const subtractDate = () => {
     const startNewYearDate = new Date("1 Jan 2023")
     const currentDate = new Date()
-    // const a = new Date(currentDate - startNewYearDate)
+
     const diff = (startNewYearDate - currentDate) / 1000
-    const minutes = Math.floor(diff / 60) % 60
-    const hours = Math.floor(diff / 3600) % 24
     const days = Math.floor(diff / 3600 / 24)
+    const hours = Math.floor(diff / 3600) % 24
+    const minutes = Math.floor(diff / 60) % 60
     const seconds = Math.floor(diff % 60)
 
-
-    console.log(minutes)
+    daysSelector.innerHTML = days.toString()
+    hoursSelector.innerHTML = hours.toString()
+    minsSelector.innerHTML = minutes.toString()
+    secondsSelector.innerHTML = seconds.toString()
 }
+
 setInterval(subtractDate, 1000)
-
-// const intr = setInterval(() => {
-//     secondsSelector.textContent = state += 1
-//     start++
-//     if (start === 5) {
-//         console.log('stop')
-//         clearInterval(intr)
-//     }
-// }, 1000)
-
