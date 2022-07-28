637938792930e0e2c84ac5f48103778edc819fb8
Noch4nce <m4rgera123@mail.ru>
2022-07-29 01:39:06 +0500

    add card

--- Notes/script.js
+++ Notes/script.js
@@ -1,1 +1,25 @@
-console.log('')
+const sidebarButtonSelector = document.querySelector('.sidebar_btn')
+const cardsContentSelector = document.querySelector('.card_content')
+
+const addCard = () => {
+	const cardBlock = document.createElement('div')
+	cardBlock.className = 'card_block'
+
+	cardBlock.innerHTML = `
+		<p class='card_input'>YO</p>
+		<div class='card_action'>
+			<button class='card_btn'>
+				<img src='./assets/bin.png' alt='trash' />
+			</button>
+			<button class='card_btn'>
+				<img src='./assets/pencil.png' alt='edit' />
+			</button>
+		</div>
+	`
+
+	cardsContentSelector.append(cardBlock)
+}
+
+sidebarButtonSelector.addEventListener('click', () => {
+	addCard()
+})
