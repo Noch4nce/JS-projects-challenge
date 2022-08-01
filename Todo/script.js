27f38aed80a5342c56e4c55252f199533449de84
Noch4nce <m4rgera123@mail.ru>
2022-08-01 16:38:56 +0500

    refactor add todo

--- Todo/script.js
+++ Todo/script.js
@@ -2,21 +2,19 @@ const formSelector = document.querySelector('.todo_form')
 const inputSelector = document.querySelector('.todo_input')
 const todoContainerSelector = document.querySelector('.todo_container')
 
-const addTodo = (target) => {
-	const inputValue = target.value
-
+const addTodo = (inputValue) => {
 	const todoBlock = document.createElement('li')
 	todoBlock.innerHTML = inputValue
 
-	todoContainerSelector.append(todoBlock)
+	todoContainerSelector.appendChild(todoBlock)
 	formSelector.reset()
 }
 
+formSelector.addEventListener('submit', (event) => {
+	event.preventDefault()
+	const todoText = inputSelector.value
 
-inputSelector.addEventListener('keypress', (event) => {
-	if (event.key === 'Enter') {
-		const target = event.target
-	    console.log(event.target.value, 'qwe')
-		addTodo(target)
+	if (todoText) {
+		addTodo(todoText)
 	}
 })
