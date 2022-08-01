2dc556ec80a866cb745e22f95a79356aec4cd11f
Noch4nce <m4rgera123@mail.ru>
2022-08-02 00:31:31 +0500

    ls

--- Todo/script.js
+++ Todo/script.js
@@ -2,17 +2,54 @@ const formSelector = document.querySelector('.todo_form')
 const inputSelector = document.querySelector('.todo_input')
 const todoContainerSelector = document.querySelector('.todo_container')
 
-const addTodo = (inputValue) => {
-	const todoBlock = document.createElement('li')
-	todoBlock.innerHTML = inputValue
+const textData = []
+let todoIds = 0
 
-	todoContainerSelector.appendChild(todoBlock)
-	formSelector.reset()
+const init = () => {
+	const todoData = localStorage.getItem('todos')
+	const todoParse = JSON.parse(todoData)
+
+	addTodo(todoParse)
+}
+
+const addTodo = (inputValue, todoParse = '') => {
+	const todoBlock = document.createElement('li')
+
+	todoParse
+		? todoParse.forEach((todoEl) => {
+			const { todoText, todoClass } = todoEl
+
+			const todoBlock = document.createElement('li')
+			todoBlock.innerHTML = todoText
+
+			if (todoClass) {
+				todoBlock.classList.add(todoClass)
+			}
+
+			textData.push({
+				id: todoIds,
+				todoText: inputValue,
+				todoClass: ''
+			})
+			todoIds++
+			todoContainerSelector.appendChild(todoBlock)
+		})
+		: ''
+	console.log(inputValue, "inputValue")
+	console.log(Boolean(inputValue), "inputValue")
+	if (inputValue) {
+		todoBlock.id = String(todoIds)
+		todoBlock.innerHTML = inputValue
+
+		todoContainerSelector.appendChild(todoBlock)
+		formSelector.reset()
+	}
 
 	todoBlock.addEventListener('click', (event) => {
 		const currentTodo = event.target
+		const currentTodoId = currentTodo.id
 
-		todoComplete(currentTodo)
+		todoComplete(currentTodo, currentTodoId)
 	})
 
 	todoBlock.addEventListener('contextmenu', (event) => {
@@ -21,10 +58,19 @@ const addTodo = (inputValue) => {
 
 		deleteTodo(currentTodo)
 	})
+
+	console.log(textData, 'textData')
+	localStorage.setItem('todos', JSON.stringify(textData))
 }
 
-const todoComplete = (currentTodo) => {
+const todoComplete = (currentTodo, currentTodoId) => {
 	currentTodo.classList.toggle('todo_complete')
+
+	textData[currentTodoId].todoClass
+		? (textData[currentTodoId].todoClass = '')
+		: (textData[currentTodoId].todoClass = 'todo_complete')
+
+	localStorage.setItem('todos', JSON.stringify(textData))
 }
 
 const deleteTodo = (currentTodo) => {
@@ -33,9 +79,11 @@ const deleteTodo = (currentTodo) => {
 
 formSelector.addEventListener('submit', (event) => {
 	event.preventDefault()
-	const todoText = inputSelector.value
+	const inputValue = inputSelector.value
 
-	if (todoText) {
-		addTodo(todoText)
+	if (inputValue) {
+		addTodo(inputValue)
 	}
 })
+
+init()
