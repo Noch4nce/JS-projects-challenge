36e7a94ce23ee61d211f86560620535fe8c26749
Noch4nce <m4rgera123@mail.ru>
2022-08-05 19:21:10 +0500

    user repos

--- Github-profiles/script.js
+++ Github-profiles/script.js
@@ -4,7 +4,8 @@ const mainContainerSelector = document.querySelector('.gp_main-container')
 const gpFormSelector = document.querySelector('.gp_form')
 const gpInputSelector = document.querySelector('.gp_input')
 
-const init = () => {
+const init = async () => {
+	// getUserRepos().then((data) => createUserRepos(data))
 	getGithubProfile().then((data) => createUserCard(data))
 }
 
@@ -19,11 +20,25 @@ const getGithubProfile = async (userInputName) => {
 	return result
 }
 
-const createUserCard = (data) => {
+const getUserRepos = async (userInputName) => {
+	const defaultUserName = 'Noch4nce'
+	const userName = userInputName ? userInputName : defaultUserName
+
+	const response = await fetch(GITHUB_PROFILES_API + userName + '/repos')
+	const result = await response.json()
+
+	console.log(result, 'getUserRepos')
+	return result
+}
+
+const createUserCard = async (data) => {
 	const { avatar_url, bio, followers, following, public_repos, name } = data
 	mainContainerSelector.innerHTML = ''
 	const userCard = document.createElement('div')
 	userCard.classList.add('gp_content')
+	const qwe = await test()
+	const asd = createUserRepos(qwe)
+	console.log(qwe, 'ASD')
 
 	userCard.innerHTML = `
 		<div>
@@ -42,15 +57,46 @@ const createUserCard = (data) => {
 				<li>${following} <strong>Following</strong></li>
 				<li>${public_repos} <strong>Repos</strong></li>
 			</ul>
-<!--			<div className='gp_links'>-->
-<!--				<a href="/">link</a>-->
-<!--				<a href="/">link</a>-->
-<!--				<a href="/">link</a>-->
-<!--			</div>-->
+			<div class='gp_links'>
+			</div>
 		</div>
 	`
 
 	mainContainerSelector.appendChild(userCard)
+	const link = document.querySelector('.gp_links')
+	link.appendChild(asd)
+	console.log(link, "link")
+
+}
+
+const test = async () => {
+	const asd = await getUserRepos()
+
+	return asd
+}
+
+const createUserRepos = (data) => {
+	const div = document.createElement('div')
+
+	data.forEach((dataEl, i) => {
+		const { name, html_url } = dataEl
+
+		if (i < 10) {
+			const linkNode = document.createElement('a')
+
+			linkNode.href = html_url
+			linkNode.innerText = name
+			linkNode.target = '_blank'
+
+			div.appendChild(linkNode)
+		} else {
+			return
+		}
+	})
+
+	console.log(div, 'DATA')
+
+	return div
 }
 
 gpFormSelector.addEventListener('submit', (event) => {
