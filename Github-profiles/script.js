0a7a57c35dccac5c1d2c63a6d7eed53ef0509869
Noch4nce <m4rgera123@mail.ru>
2022-08-05 16:39:50 +0500

    create user card

--- Github-profiles/script.js
+++ Github-profiles/script.js
@@ -1,7 +1,9 @@
 const GITHUB_PROFILES_API = 'https://api.github.com/users/'
 
+const mainContainerSelector = document.querySelector('.gp_main-container')
+
 const init = () => {
-	getGithubProfile()
+	getGithubProfile().then((data) => createUserCard(data))
 }
 
 const getGithubProfile = async () => {
@@ -11,6 +13,41 @@ const getGithubProfile = async () => {
 	const result = await response.json()
 
 	console.log(result, 'result')
+	return result
+}
+
+const createUserCard = (data) => {
+	const { avatar_url, bio, followers, following, public_repos, name } = data
+	mainContainerSelector.innerHTML = ''
+	const userCard = document.createElement('div')
+	userCard.classList.add('gp_content')
+
+	userCard.innerHTML = `
+		<div>
+			<img
+				class='gp_avatar'
+				src="${avatar_url}"
+				alt="avatar"
+			/>
+		</div>
+	
+		<div class='gp_info'>
+			<h2>${name}</h2>
+			<p>${bio}</p>
+			<ul class='gp_stats'>
+				<li>${followers} <strong>Followers</strong></li>
+				<li>${following} <strong>Following</strong></li>
+				<li>${public_repos} <strong>Repos</strong></li>
+			</ul>
+<!--			<div className='gp_links'>-->
+<!--				<a href="/">link</a>-->
+<!--				<a href="/">link</a>-->
+<!--				<a href="/">link</a>-->
+<!--			</div>-->
+		</div>
+	`
+
+	mainContainerSelector.appendChild(userCard)
 }
 
 init()
