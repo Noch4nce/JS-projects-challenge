61daa502fea1d94949665f55393c741a47a27627
Noch4nce <m4rgera123@mail.ru>
2022-08-06 15:31:57 +0500

    draw mousedown

--- Drawing/script.js
+++ Drawing/script.js
@@ -8,14 +8,21 @@ let y = 60
 const drawingCircle = (x, y) => {
 	ctx.beginPath()
 	ctx.arc(x, y, size, 0, 2 * Math.PI)
-	ctx.stroke()
+	ctx.fill()
 }
 
-const draw = () => {
-	ctx.clearRect(0, 0, canvasSelector.width, canvasSelector.height)
+// const draw = () => {
+// 	ctx.clearRect(0, 0, canvasSelector.width, canvasSelector.height)
+//
+// 	drawingCircle(x, y)
+// 	requestAnimationFrame(draw)
+// }
+
+canvasSelector.addEventListener('mousedown', (event) => {
+	const x = event.offsetX
+	const y = event.offsetY
 
 	drawingCircle(x, y)
-	requestAnimationFrame(draw)
-}
+})
 
-draw()
+// draw()
