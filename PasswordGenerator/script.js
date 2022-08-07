caad7f0f39830beb72e441caf27718081b3410cf
Noch4nce <m4rgera123@mail.ru>
2022-08-07 19:02:47 +0500

    generated upper case pw

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -1,10 +1,13 @@
 const lowerCaseSelector = document.getElementById('toggle-button_lower')
+const upperCaseSelector = document.getElementById('toggle-button_upper')
 const generateBtnSelector = document.getElementById('pw_generate')
 const pwInputSelector = document.getElementById('pw_pass')
 
 let pwLength = 5
 let isLowerCaseChecked = false
+let isUpperCaseChecked = false
 const lowerCharacters = 'abcdefghijklmnopqrstuvwxyz'
+const upperCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 // let asd = (Math.random())
 // let qwe = (0.17749028285925537 * 26)
@@ -24,13 +27,33 @@ const generatedLowerCase = () => {
 	pwInputSelector.innerText = res
 }
 
+const generatedUpperCase = () => {
+	let res = ''
+
+	for (let i = 0; i < pwLength; i++) {
+		const randomCharacter =
+			upperCharacters[Math.floor(Math.random() * upperCharacters.length)]
+
+		res = res += randomCharacter
+	}
+
+	pwInputSelector.innerText = res
+}
+
 lowerCaseSelector.addEventListener('click', (e) => {
 	isLowerCaseChecked = e.target.checked
+})
+
+upperCaseSelector.addEventListener('click', (e) => {
+	isUpperCaseChecked = e.target.checked
 })
 
 generateBtnSelector.addEventListener('click', () => {
 	if (isLowerCaseChecked) {
 		generatedLowerCase()
 	}
-})
 
+	if (isUpperCaseChecked) {
+		generatedUpperCase()
+	}
+})
