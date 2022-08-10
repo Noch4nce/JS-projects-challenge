48f125abb6e7cab0e4be5d5b5f97b518066adca8
Noch4nce <m4rgera123@mail.ru>
2022-08-10 17:43:07 +0500

    submit city

--- Weather/script.js
+++ Weather/script.js
@@ -1,17 +1,41 @@
 const API_KEY = '5ef6cbd386274b5949af86a25342cc9d'
+const weatherFormSelector = document.querySelector('.weather_form')
+const weatherInputSelector = document.querySelector('.weather_input')
 
 const init = () => {
-	getWeatherData().then((data) => {'data'})
+	const defaultCity = 'London'
+
+	getWeatherData(defaultCity).then((data) => {
+		createWeatherContent(data)
+	})
 }
 
-const getWeatherData = async () => {
+const getWeatherData = async (city) => {
 	const response = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`
+		`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
 	)
 	const result = await response.json()
 
-	console.log(result, "RES")
+	console.log(result, 'RES')
 	return result
 }
+
+const createWeatherContent = (weatherData) => {
+	console.log(weatherData, 'QEQWE')
+}
+
+weatherFormSelector.addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	const inputValue = weatherInputSelector.value
+
+	if (inputValue) {
+		getWeatherData(inputValue).then((data) => {
+			createWeatherContent(data)
+		})
+	}
+
+	weatherInputSelector.value = ''
+})
 
 // init()
